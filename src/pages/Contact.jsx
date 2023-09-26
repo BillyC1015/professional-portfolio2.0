95adcd789c565f65b310fe07b280f674f321e3e0
Billy Cody <william.cody1015@gmail.com>
2023-09-25 20:43:54 -0400

    got everything but projects working

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,11 @@
 export default function Contact(){
     return(
         <>
-        <p>hi</p>
+       <p>Mechanicsburg, PA</p>
+        <p>Email: william.cody1015@gmail.com
+            <p>Phone: 717-557-2662</p>
+            <p>Linkedin: https://www.linkedin.com/in/williamcody15/</p>
+        </p>
         </>
     )
 }
