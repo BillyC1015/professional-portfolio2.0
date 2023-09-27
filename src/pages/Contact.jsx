087083a6a94ba35f9eba4e15d3db74cb30a84eea
Billy Cody <william.cody1015@gmail.com>
2023-09-27 17:14:18 -0400

    added github to contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -5,6 +5,7 @@ export default function Contact(){
         <p>Email: william.cody1015@gmail.com
             <p>Phone: 717-557-2662</p>
             <p>Linkedin: https://www.linkedin.com/in/williamcody15/</p>
+            <p>Github: https://github.com/BillyC1015</p>
         </p>
         </>
     )
