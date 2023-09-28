bbb9d366a5ab3dd5963daac9582ecfdf6631df7d
Billy Cody <william.cody1015@gmail.com>
2023-09-27 22:43:37 -0400

    uploaded all projects to portfolio

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -2,7 +2,7 @@ function Project(props) {
     return (
       <div className="project">
         <div className="card" style={{width: "18rem"}}>
-  <img src={props.image} className="card-img-top" alt="..."/>
+  {/* <img src={props.image} className="card-img-top" alt="..."/> */}
   <div className="card-body">
     <h5 className="card-title">{props.name}</h5>
     <p className="card-text">{props.description}</p>
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,4 +1,5 @@
 import Project from "../components/Project.jsx";
+// import CritterTracker from "../assets/CritterTrackerDemo.gif";
 
 function Portfolio() {
     let projects=[
@@ -7,21 +8,21 @@ function Portfolio() {
             description: "An app where users can find a list of breweries and national parks within a certain area.",
             github: "https://github.com/nolannaphys/park-brew",
             url: "https://nolannaphys.github.io/park-brew/",
-            image: "path to image here"
+            // image: "path to image here"
         },
         {
             name: "Critter Tracker", 
             description: "Sort of a real-world Pokedex, users can upload images of animals they see in the wild and they load onto a map",
             github: "github url here",
             url: "https://critter-tracker-p2-d4b8f730eb59.herokuapp.com/",
-            image: "path to image here"
+            // image: ""
         },
         {
             name: "Movie Vault", 
             description: "A database of movies that users can search through and save ones they want to watch later",
             github: "https://github.com/nolannaphys/movie-vault",
             url: "https://movie-vault-a6d86baeac1b.herokuapp.com/",
-            image: "path to image here"
+            // image: "path to image here"
         },
     ]
     return (
