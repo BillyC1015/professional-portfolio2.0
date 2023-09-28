7f9e3e0222093cbccf26c1000b65e29622c70767
Billy Cody <william.cody1015@gmail.com>
2023-09-27 20:49:56 -0400

    updated portfolio section with project names and links

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -6,8 +6,8 @@ function Project(props) {
   <div className="card-body">
     <h5 className="card-title">{props.name}</h5>
     <p className="card-text">{props.description}</p>
-    <a href={props.url} className="btn btn-primary">one for live site</a>
-    <a href={props.github} className="btn btn-primary">one for github, replace # in href with url</a>
+    <a href={props.url} className="btn btn-primary">Live Site</a>
+    <a href={props.github} className="btn btn-primary">Github Repo</a>
   </div>
 </div>
       </div>
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -3,24 +3,24 @@ import Project from "../components/Project.jsx";
 function Portfolio() {
     let projects=[
         {
-            name: "Project 1", 
-            description: "describe here",
-            github: "github url here",
-            url: "url to live site here",
+            name: "Park Brew", 
+            description: "An app where users can find a list of breweries and national parks within a certain area.",
+            github: "https://github.com/nolannaphys/park-brew",
+            url: "https://nolannaphys.github.io/park-brew/",
             image: "path to image here"
         },
         {
-            name: "Project 2", 
-            description: "describe here",
+            name: "Critter Tracker", 
+            description: "Sort of a real-world Pokedex, users can upload images of animals they see in the wild and they load onto a map",
             github: "github url here",
-            url: "url to live site here",
+            url: "https://critter-tracker-p2-d4b8f730eb59.herokuapp.com/",
             image: "path to image here"
         },
         {
-            name: "Project 3", 
-            description: "describe here",
-            github: "github url here",
-            url: "url to live site here",
+            name: "Movie Vault", 
+            description: "A database of movies that users can search through and save ones they want to watch later",
+            github: "https://github.com/nolannaphys/movie-vault",
+            url: "https://movie-vault-a6d86baeac1b.herokuapp.com/",
             image: "path to image here"
         },
     ]
