e8bb0422d1f6ac7cc243dea6ab3f0b06fb231808
Billy Cody <william.cody1015@gmail.com>
2023-09-27 20:35:19 -0400

    some style changes and fixed about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,7 +5,7 @@ export default function About(){
         <div className="about">
         <h1>About</h1>
     
-        <h4>I am a recent graduate of The University of Pennsylvania's Full Stack Coding Bootcamp and am looking to prove myself in the job field. I have a background in sales and decided I wanted a career change.  </h4>
+        <h4>I am a recent graduate of the University of Pennsylvania's coding bootcamp, aiming to transfer from sales focused roles to a more software development and web design oriented position. I am looking to put my new skills to the test and further develop them plus utilize the skills that I already bring to the table. With this recent certification along with a degree from Temple University and a background in sales roles, I intend to add value to a new company and grow my career in coding. </h4>
 
         <div className="profile-pic">
         <img src={profilePic} alt="me sitting under a tree in the desert"/></div>
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,11 +2,11 @@ export default function Contact(){
     return(
         <>
        <p>Mechanicsburg, PA</p>
-        <p>Email: william.cody1015@gmail.com
+        <p>Email: william.cody1015@gmail.com </p>
             <p>Phone: 717-557-2662</p>
             <p>Linkedin: https://www.linkedin.com/in/williamcody15/</p>
             <p>Github: https://github.com/BillyC1015</p>
-        </p>
+       
         </>
     )
 }
