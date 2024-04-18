294bd19eb1f45ffac9b14fc594c1e9ba49ccaebd
Billy Cody <william.cody1015@gmail.com>
2024-04-18 18:06:56 -0400

    added email?

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,27 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const ButtonMailto = ({ mailto, label }) => {
+    return (
+        <Link
+            to='#'
+            onClick={(e) => {
+                window.location.href = mailto;
+                e.preventDefault();
+            }}
+        >
+            {label}
+        </Link>
+    );
+};
+
+export default ButtonMailto;
+
 export default function Contact(){
     return(
         <>
        <p>Mechanicsburg, PA</p>
-        <p><a href="mailto:william.cody1015@gmail.com">Email: william.cody1015@gmail.com</a> </p>
+       <ButtonMailto label="Write me an E-Mail" mailto="mailto:no-reply@example.com" />
             <p>Phone: 717-557-2662</p>
             <p>Linkedin: https://www.linkedin.com/in/williamcody15/</p>
             <p>Github: https://github.com/BillyC1015</p>
