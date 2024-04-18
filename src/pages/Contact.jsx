3cc4c4d3fe699fe2ed66eda3179b0b2db2e19597
Billy Cody <william.cody1015@gmail.com>
2024-04-18 18:02:45 -0400

    missed a character

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,7 +2,7 @@ export default function Contact(){
     return(
         <>
        <p>Mechanicsburg, PA</p>
-        <p>><a href="mailto:william.cody1015@gmail.com">Email: william.cody1015@gmail.com</a> </p>
+        <p><a href="mailto:william.cody1015@gmail.com">Email: william.cody1015@gmail.com</a> </p>
             <p>Phone: 717-557-2662</p>
             <p>Linkedin: https://www.linkedin.com/in/williamcody15/</p>
             <p>Github: https://github.com/BillyC1015</p>
